48ff9bc847697bb220063dc3f70253ce450068e8
Hernan Alvarez Caballero <hernandavidc@hotmail.com>
2021-11-23 19:55:14 -0500

    Update server.js

--- vue-auth/server.js
+++ vue-auth/server.js
@@ -6,7 +6,7 @@ var app = express();
 
 app.use( cors() );
 app.use( express.json() );
-app.use( express.static('dist') );
+app.use( express.static('dist') );  //OJO el dist esta ignorado en el fichero .gitignore
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
